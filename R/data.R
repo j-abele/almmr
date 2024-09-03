884b404a85e939b38f039de686972620761ca46c
j-abele <jonas.abele@gmx.de>
2024-09-03 23:43:06 +0200

    minor changes descritption

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Digital elevation model
 #'
-#' @format ## `dem`
+#' @format `dem`
 #' Digital elevation model of the Heuneburg region (5 km rectangle from Heuneburg)
 #' with a resolution of 25 m, aggregated from high resolution LiDAR files from the
 #' Landesamt für Geoinformation und Landentwicklung Baden-Württemberg.
